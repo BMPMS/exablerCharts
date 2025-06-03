95daab2ba6071b100956d283fd8e4c611ef5f8b9
BMPMS <milesbryony@googlemail.com>
2025-06-03 12:03:58 +0100

    fix: log and linear

--- js/d3Charts.js
+++ js/d3Charts.js
@@ -3,30 +3,29 @@
 const scatterChart = ()  => {
 
     let props = {};
+    let title = "";
     let chartWidth = 0;
     let chartHeight = 0;
     let svg = undefined;
 
     const chart = (svg) => {
 
-        const margin = { left: 80, right: 20, top: 50, bottom: 60 };
-        const { data, dotAttributes, fontSize, labels, xVar, yVar } = props;
+        const margin = { left: 100, right: 30, top: 70, bottom: 60 };
+        const { data, dotAttributes, fontSize, format, labels, scales, xVar, yVar } = props;
 
         const xExtent = d3.extent(data, (d) => +d[xVar]);
 
-        debugger;
-        const xScale = d3
-            .scaleLinear()
+        const xScale = d3[scales.x]()
             .domain(xExtent)
-            .range([0, chartWidth - margin.left - margin.right]);
+            .range([0, chartWidth - margin.left - margin.right]).nice();
 
         const yExtent = d3.extent(data, (d) => +d[yVar]);
 
-        const yScale = d3
-            .scaleLinear()
+        const yScale = d3[scales.y]()
             .domain(yExtent)
-            .range([chartHeight - margin.top - margin.bottom, 0]);
+            .range([chartHeight - margin.top - margin.bottom, 0]).nice();
 
+        let titleLabel = svg.select(".titleLabel");
         let xAxisLabel = svg.select(".xAxisLabel");
         let yAxisLabel = svg.select(".yAxisLabel");
         let xAxis = svg.select(".xAxis");
@@ -34,11 +33,20 @@ const scatterChart = ()  => {
 
         if(xAxis.node() === null){
             // append if initial draw
+            titleLabel = svg.append("text").attr("class","titleLabel");
             xAxisLabel = svg.append("text").attr("class","xAxisLabel");
             yAxisLabel = svg.append("text").attr("class","yAxisLabel");
             xAxis = svg.append("g").attr("class","xAxis");
             yAxis = svg.append("g").attr("class","yAxis");
         }
+
+        titleLabel
+            .attr("x", chartWidth / 2)
+            .attr("y", fontSize * 2)
+            .attr("text-anchor", "middle")
+            .attr("font-size", fontSize * 1.2)
+            .attr("fill", "#484848")
+            .html(title);
 
         xAxisLabel
             .attr("x", margin.left + (chartWidth - margin.left - margin.right) / 2)
@@ -61,7 +69,7 @@ const scatterChart = ()  => {
             .html(labels.yAxis);
 
         xAxis
-            .call(d3.axisBottom(xScale).tickSizeOuter(0))
+            .call(d3.axisBottom(xScale).ticks(5).tickFormat((d) => d3.format(format.x)(d)).tickSizeOuter(0))
             .attr("transform", `translate(${margin.left},${chartHeight - margin.bottom})`);
 
         xAxis.selectAll("path").style("stroke", "#A0A0A0");
@@ -77,7 +85,7 @@ const scatterChart = ()  => {
             .attr("font-size", fontSize);
 
         yAxis
-            .call(d3.axisLeft(yScale).tickSizeOuter(0))
+            .call(d3.axisLeft(yScale).ticks(5).tickSizeOuter(0))
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
         yAxis.selectAll("path").style("stroke", "#A0A0A0");
@@ -91,7 +99,7 @@ const scatterChart = ()  => {
             .attr("x", -5)
             .attr("fill", "grey")
             .attr("font-size", fontSize)
-            .text((d) => (d === 0 ? "" : d));
+            .text((d) => (d === 0 ? "" : d3.format(format.y)(d)));
 
         const { defaultFill, opacity, radius, strokeWidth } = dotAttributes;
 
@@ -120,6 +128,12 @@ const scatterChart = ()  => {
 
 
     }
+
+    chart.title =  (value) => {
+        if (!value) return title;
+        title = value;
+        return chart;
+    };
 
     chart.chartWidth =  (value) => {
         if (!value) return chartWidth;
