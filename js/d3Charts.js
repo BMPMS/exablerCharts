ba6952d444e88812f4590ca3c30d5120db6eea6c
BMPMS <milesbryony@googlemail.com>
2025-06-03 14:08:54 +0100

    fix: vertical bar

--- js/d3Charts.js
+++ js/d3Charts.js
@@ -1,4 +1,9 @@
 
+const measureWidth = (text, fontSize) => {
+    const context = document.createElement("canvas").getContext("2d");
+    context.font = `${fontSize}px Arial`;
+    return context.measureText(text).width;
+}
 
 const scatterChart = ()  => {
 
@@ -241,11 +246,12 @@ const horizontalBarChart = ()  => {
     let title = "";
     let chartWidth = 0;
     let chartHeight = 0;
-    let svg = undefined;
+    let baseSvg = undefined;
 
-    const chart = (svg) => {
+    const chart = (baseSvg) => {
 
         const margin = { left: 100, right: 30, top: 70, bottom: 60 };
+        const brushBarCurve = 2;
 
         return chart;
     };
@@ -266,6 +272,208 @@ const horizontalBarChart = ()  => {
     chart.chartHeight =  (value) => {
         if (!value) return chartHeight;
         chartHeight = value;
+        return chart;
+    };
+
+    chart.props =  (value) => {
+        if (!value) return props;
+        props = value;
+        return chart;
+    };
+
+    return chart;
+}
+
+
+const verticalBarChart = ()  => {
+
+    let props = {};
+    let title = "";
+    let chartWidth = 0;
+    let chartHeight = 0;
+    let chartClass = "";
+
+    const chart = (svg) => {
+
+        const margin = { left: 80, right: 30, top: 70, bottom: 60 };
+
+        const { data, barAttributes, fontSize, format, labels, xVar, ySort,yVar } = props;
+
+        const barData =
+            Array.from(d3.group(data, (g) => g[yVar]))
+                .reduce((acc, entry) => {
+                    acc.push({
+                        yVar: entry[0],
+                        total: d3.sum(entry[1],(s) => s[xVar])
+                    })
+                    return acc;
+                },[])
+                .sort((a,b) => d3[ySort](a.total,b.total))
+
+        const xExtent = d3.extent(barData, (d) => d.total);
+
+        const xScale = d3.scaleLinear()
+            .domain(xExtent)
+            .range([0, chartWidth - margin.left - margin.right]).nice();
+
+        const yBands =  barData.map((m) => m.yVar);
+
+        const yScale = d3.scaleBand()
+            .domain(yBands)
+            .range([chartHeight - margin.top - margin.bottom, 0]);
+
+        let titleLabel = svg.select(".titleLabel");
+        let xAxisLabel = svg.select(".xAxisLabel");
+        let yAxisLabel = svg.select(".yAxisLabel");
+        let xAxis = svg.select(".xAxis");
+        let yAxis = svg.select(".yAxis");
+
+        if(xAxis.node() === null){
+            // append if initial draw
+            titleLabel = svg.append("text").attr("class","titleLabel");
+            xAxisLabel = svg.append("text").attr("class","xAxisLabel");
+            yAxisLabel = svg.append("text").attr("class","yAxisLabel");
+            xAxis = svg.append("g").attr("class","xAxis");
+            yAxis = svg.append("g").attr("class","yAxis");
+        }
+
+        titleLabel
+            .attr("x", chartWidth / 2)
+            .attr("y", fontSize * 2)
+            .attr("text-anchor", "middle")
+            .attr("font-size", fontSize * 1.2)
+            .attr("fill", "#484848")
+            .html(title);
+
+        xAxisLabel
+            .attr("x", margin.left + (chartWidth - margin.left - margin.right) / 2)
+            .attr("y", chartHeight - fontSize)
+            .attr("text-anchor", "middle")
+            .attr("font-size", fontSize)
+            .attr("fill", "grey")
+            .html(labels.xAxis);
+
+        yAxisLabel
+            .attr(
+                "transform",
+                `translate(30,${
+                    margin.top + (chartHeight - margin.top - margin.bottom) / 2
+                }) rotate(-90)`
+            )
+            .attr("text-anchor", "middle")
+            .attr("font-size", fontSize)
+            .attr("fill", "grey")
+            .html(labels.yAxis);
+
+        xAxis
+            .call(d3.axisBottom(xScale).ticks(5).tickFormat((d) => d3.format(format.x)(d)).tickSizeOuter(0))
+            .attr("transform", `translate(${margin.left},${chartHeight - margin.bottom})`);
+
+        xAxis.selectAll("path").style("stroke", "#A0A0A0");
+
+        xAxis.selectAll("line").attr("display", "none");
+
+        xAxis
+            .selectAll("text")
+            .attr("pointer-events", "none")
+            .attr("font-weight", 300)
+            .attr("fill", "grey")
+            .attr("y", 5)
+            .attr("font-size", fontSize);
+
+        yAxis
+            .call(d3.axisLeft(yScale).ticks(5).tickSizeOuter(0))
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        yAxis.selectAll("path").style("stroke", "#A0A0A0");
+
+        yAxis.selectAll("line").attr("display", "none");
+
+        yAxis
+            .selectAll("text")
+            .attr("pointer-events", "none")
+            .attr("font-weight", 300)
+            .attr("x", -5)
+            .attr("fill", "grey")
+            .attr("font-size", fontSize);
+
+        const {barHeight, cornerRadius, fill} = barAttributes;
+
+
+        const barGroup = svg
+            .selectAll(".barGroup")
+            .data(barData)
+            .join((group) => {
+                const enter = group.append("g").attr("class", "barGroup");
+                enter.append("rect").attr("class", "barRect");
+                enter.append("text").attr("class", "barLabel");
+                enter.append("clipPath").attr("class", "barClipPath")
+                     .append("rect").attr("class", "clipRect");
+                return enter;
+            });
+
+        barGroup.attr("transform", `translate(${margin.left},${margin.top})`);
+
+        barGroup
+            .select(".barClipPath")
+            .attr("id", (d, i) => `clipRect${chartClass}${i}`);
+
+        barGroup
+            .select(".clipRect")
+            .attr("x", 0)
+            .attr("y", (d) => yScale(d.yVar))
+            .attr("width", (d) => xScale(d.total))
+            .attr("height", barHeight);
+
+        barGroup
+            .select(".barRect")
+            .attr("clip-path", (d, i) => `url(#clipRect${chartClass}${i})`)
+            .attr("rx", cornerRadius)
+            .attr("ry", cornerRadius)
+            .attr("x", -cornerRadius)
+            .attr("y", (d) => yScale(d.yVar))
+            .attr("width", (d) => xScale(d.total) + cornerRadius)
+            .attr("height", barHeight - 2)
+            .attr("fill", fill);
+
+        barGroup
+            .select(".barLabel")
+            .attr("x", (d) => xScale(d.total))
+            .attr("dx", (d) => measureWidth(d3.format(format.x)(d.total),fontSize) + 10 < xScale(d.total) ? -3 : 3)
+            .attr("y", (d) => yScale(d.yVar)  + barHeight/2)
+            .attr("text-anchor", (d) => measureWidth(d3.format(format.x)(d.total),fontSize) + 10 < xScale(d.total) ? "end" : "start")
+            .style("dominant-baseline","middle")
+            .attr("font-weight", 300)
+            .attr("fill", (d) => measureWidth(d3.format(format.x)(d.total),fontSize) + 10 < xScale(d.total) ? "white" : "grey")
+            .attr("font-size", fontSize)
+            .text((d) => d3.format(format.x)(d.total));
+
+
+        return chart;
+    };
+
+
+    chart.title =  (value) => {
+        if (!value) return title;
+        title = value;
+        return chart;
+    };
+
+    chart.chartWidth =  (value) => {
+        if (!value) return chartWidth;
+        chartWidth = value;
+        return chart;
+    };
+
+    chart.chartHeight =  (value) => {
+        if (!value) return chartHeight;
+        chartHeight = value;
+        return chart;
+    };
+
+    chart.chartClass =  (value) => {
+        if (!value) return chartClass;
+        chartClass = value;
         return chart;
     };
 
